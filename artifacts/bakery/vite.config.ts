9998b723e6266486973737a1ca4e187d4c7361d2
ssalishadrach31 <ssalishadrach31@gmail.com>
2026-05-14 14:34:14 +0000

    fix: remove frontend PORT dependency

--- artifacts/bakery/vite.config.ts
+++ artifacts/bakery/vite.config.ts
@@ -38,7 +38,6 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
     allowedHosts: true,
     fs: {
       strict: true,
